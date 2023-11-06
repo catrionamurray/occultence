speed up binning by splitting into days, return list of BLS light curves, small bug fixes, +hyper param kw, add code for masking an existing transit,

occultence/binning/binning.py
@@ -1,8 +1,20 @@
 from ..imports import *
 
-def bin(self, dt, bin_func=np.nanmedian, **kw):
+def bin(self, dt, bin_func=np.nanmedian, split=True, **kw):
     ts = TimeSeries(self.timelike)
-    bin_ts = aggregate_downsample(ts, time_bin_size=dt, aggregate_func=bin_func, **kw)
+
+    if split:
+        bin_ts = []
+        i_split, _ = self.split_time()
+        for i0, i1 in zip(i_split[:-1], i_split[1:]):
+            if i0 == 0:
+                bin_ts = aggregate_downsample(ts[i0:i1], time_bin_size=dt, aggregate_func=bin_func, **kw)
+            else:
+                bts = aggregate_downsample(ts[i0:i1], time_bin_size=dt, aggregate_func=bin_func, **kw)
+                for b in bts:
+                    bin_ts.add_row(b)
+    else:
+        bin_ts = aggregate_downsample(ts, time_bin_size=dt, aggregate_func=bin_func, **kw)
 
     binned_lc = self._create_copy()
     binned_lc.timelike = {}
@@ -25,3 +37,21 @@ def bin(self, dt, bin_func=np.nanmedian, **kw):
 
     binned_lc._set_name(binned_lc.name + "_bin")
     return binned_lc
+
+def split_time(self, split=0.5 * u.d):
+    t0 = self.time[0]
+    prev_obs_night = 0
+    obs_nights, obs_nights_indexes = [], []
+
+    for i, t in enumerate(self.time[1:]):
+        if (t - t0) >= split:
+            obs_nights.append(self.time[prev_obs_night:i + 1])
+            obs_nights_indexes.append(i + 1)
+            prev_obs_night = i + 1
+        t0 = t
+
+    obs_nights_indexes.insert(0,0)
+    obs_nights_indexes.append(len(self.time))
+
+    return obs_nights_indexes, obs_nights
+

occultence/injection/transit.py
@@ -252,7 +252,8 @@ def generate_planet_distribution(nfake, m_s, r_s, per=[np.log10(0.5),np.log10(10
 
 def create_lots_of_transit_params(self, nfake=1000, R_star=None, M_star=None, T_eff=None, SpT=None,
                                  minimum_planet_radius=0.5 * u.R_earth, maximum_planet_radius=3 * u.R_earth,
-                                 minimum_period=0.5 * u.d, maximum_period=30 * u.d, store_planets=True, fname=None,):
+                                 minimum_period=0.5 * u.d, maximum_period=30 * u.d, store_planets=True, fname=None,
+                                  **kw):
 
     if R_star is None or M_star is None:
         if "R_star" in self.metadata and "M_star" in self.metadata:
@@ -275,7 +276,7 @@ def create_lots_of_transit_params(self, nfake=1000, R_star=None, M_star=None, T_
     if store_planets:
         params = generate_planet_distribution(nfake, M_star, R_star,
                                               radius=[minimum_planet_radius.to_value('R_earth'), maximum_planet_radius.to_value('R_earth')],
-                                              per=[minimum_period.to_value('d'), maximum_period.to_value('d')])
+                                              per=[minimum_period.to_value('d'), maximum_period.to_value('d')], **kw)
         transit_depth, transit_duration, transit_epoch, transit_a, transit_a_Rs = [],[],[],[],[]
         for p in range(len(params[0])):
             a_Rs = semi_major_axis(params[0][p] * u.d, M_star, R_star).decompose()
@@ -316,16 +317,16 @@ def create_lots_of_transit_params(self, nfake=1000, R_star=None, M_star=None, T_
 def inject_lots_of_transits(self, nfake=1000, R_star=None, M_star=None, T_eff=None, SpT=None, ld = [0.385, 0.304],
                             minimum_planet_radius=0.5 * u.R_earth, maximum_planet_radius=3 * u.R_earth,
                             minimum_period=0.5 * u.d, maximum_period=30 * u.d, store_planets=True, fname=None,
-                            pool=True, ncores=1):
+                            pool=True, ncores=1, **kw):
 
     planets = self.create_lots_of_transit_params(nfake=nfake, R_star=R_star, M_star=M_star, T_eff=T_eff, SpT=SpT,
                                minimum_planet_radius=minimum_planet_radius, maximum_planet_radius=maximum_planet_radius,
                                minimum_period=minimum_period, maximum_period=maximum_period,
-                               store_planets=store_planets, fname=fname)
+                               store_planets=store_planets, fname=fname, **kw)
 
     if pool:
         lcs_with_transits = self.pool_inject_transit(planets['logP'], planets['phase'], planets['cosi'], planets['r_p'],
-                                ld, ncores, R_star=R_star, M_star=M_star)
+                                ld, ncores, R_star=R_star, M_star=M_star, **kw)
     else:
         lcs_with_transits = []
         # for logp, phase, cosi, rp in zip(planets['logP'], planets['phase'], planets['cosi'], planets['r_p']):

occultence/lightcurve_detrending/gp.py
@@ -39,6 +39,8 @@ def gp(x,
        x_original,
        rotation_period=None,
        rotation_amp=None,
+       sqexp_metric=0.5,
+       amp_metric=None,
        plot=False,
        figsize=(12,4),
        verbose=False
@@ -48,8 +50,11 @@ def gp(x,
 
     # try:
     # jitter = george.kernels.ConstantKernel(log_constant=np.log(np.nanmedian(yerr)))
-    amp = george.kernels.ConstantKernel(log_constant=np.log(np.std(y)))
-    sqexp = george.kernels.ExpSquaredKernel(0.5, metric_bounds={'log_M_0_0':(np.log(0.01),np.log(1000))})
+    if amp_metric is None:
+        amp = george.kernels.ConstantKernel(log_constant=np.log(np.std(y)))
+    else:
+        amp = george.kernels.ConstantKernel(log_constant=np.log(amp_metric))
+    sqexp = george.kernels.ExpSquaredKernel(sqexp_metric, metric_bounds={'log_M_0_0': (np.log(0.01), np.log(1000))})
 
     # If the user has passed a rotation period to the GP then use quasi-periodic kernel, otherwise use a squared
     # exponential kernel.
@@ -59,7 +64,7 @@ def gp(x,
             therefore we will use 0.1 as the starting value"""
             cheerfully_suggest(message)
             rotation_amp = 0.1
-        sinexp = george.kernels.ExpSine2Kernel(gamma=rotation_amp,log_period=np.log(rotation_period))
+        sinexp = george.kernels.ExpSine2Kernel(gamma=rotation_amp, log_period=np.log(rotation_period))
         gpkernel = amp * (sqexp*sinexp)
         kernel = "quasi-periodic"
         if verbose:
@@ -70,7 +75,7 @@ def gp(x,
         if verbose:
             print(f"Fitting rotation with square-exponential GP...")
 
-    gaussproc = george.GP(gpkernel,white_noise=np.log(np.nanmedian(yerr)),fit_white_noise=True)
+    gaussproc = george.GP(gpkernel, white_noise=np.log(np.nanmedian(yerr)), fit_white_noise=True)
     gaussproc.compute(x, yerr)
 
     if verbose: