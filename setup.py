b8e93be3161ffbe127d1c92bc5d41e3cc03be52e
catrionamurray <Catriona.Murray@colorado.edu>
2024-04-18 08:25:47 -0600

    added pytransit model version limit

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ setup(
         "fitsio",
         "george",
         "celerite",
-        "pytransit<2.6.0",
+        "pytransit==2.5.20",
         "scikit-learn",
         "emcee",
         "corner",
